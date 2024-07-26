083d432f9cb5775a6c73ce0d2698fc99aa926fe6
akuataya <akuataja@gmail.com>
2024-07-26 19:39:45 +0200

    Add icon to logo

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
-import Controls from "./components/Controls";
 import Result from "./components/Result";
+import TraditionalMethods from "./components/TraditionalMethods";
+// import MolecularMethods from "./components/MolecularMethods";
 
 const App = () => {
   const [radioSelections, setRadioSelections] = useState({
@@ -17,21 +18,63 @@ const App = () => {
   // Render
   return (
     <>
-      {/* Logo */}
-      <h1>Grapes of Staph</h1>
+      <header>
+        <svg
+          width="13"
+          height="13"
+          viewBox="0 0 64 64"
+          fill="none"
+          xmlns="http://www.w3.org/2000/svg">
+          <ellipse
+            cx="32"
+            cy="45.4286"
+            rx="16"
+            ry="16"
+            transform="rotate(180 32 45.4286)"
+            fill="#9D70CC"
+          />
+          <ellipse
+            cx="16"
+            cy="18"
+            rx="16"
+            ry="16"
+            transform="rotate(180 16 18)"
+            fill="#9D70CC"
+          />
+          <ellipse
+            cx="48"
+            cy="18"
+            rx="16"
+            ry="16"
+            transform="rotate(180 48 18)"
+            fill="#9D70CC"
+          />
+          <rect
+            x="22.8572"
+            y="18"
+            width="18.2857"
+            height="18.2857"
+            fill="#9D70CC"
+          />
+        </svg>
+        <h1>Grapes of Staph</h1>
+      </header>
 
       <main>
         {/* Title + result */}
         <section>
           <h2>Is it S. aureus?</h2>
-          <Result radioSelections={radioSelections} />
           {/* pass radio selections to calculate result */}
+          <Result radioSelections={radioSelections} />
         </section>
 
-        {/* Controls */}
+        {/* Methods */}
         <section className="controls">
-          <Controls onChange={handleSelectionChange} />
-          {/* onChange, Controls component can call handleSelectionChange */}
+          {/* onChange, components below can call handleSelectionChange */}
+          {/* Molecular methods */}
+          {/* <MolecularMethods onChange={handleSelectionChange} />  TODO  */}
+          {/* Traditional methods */}
+          <TraditionalMethods onChange={handleSelectionChange} />
         </section>
       </main>
     </>
