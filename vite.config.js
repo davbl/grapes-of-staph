bc651f4ad041d04e87720e0f7359053439d3af54
akuataya <akuataja@gmail.com>
2024-08-08 17:40:59 +0200

    Prepared for deployment

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/grapes-of-staph/",
   server: {
     host: "0.0.0.0",
     port: 3000, // use any port number
