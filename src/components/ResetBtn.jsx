9224d85784496c70703ee09413a9c75641f2dc22
akuataya <akuataja@gmail.com>
2024-09-11 17:35:40 +0200

    Update styles

--- src/components/ResetBtn.jsx
+++ src/components/ResetBtn.jsx
@@ -13,12 +13,12 @@ const ResetBtn = () => {
     <button type="button" className="reset-btn" onClick={handleReset}>
       <svg
         xmlns="http://www.w3.org/2000/svg"
-        width="13.5"
-        height="13.5"
+        width="14"
+        height="14"
         viewBox="0 0 24 24"
         fill="none"
         stroke="currentColor"
-        strokeWidth="2.5"
+        strokeWidth="3"
         strokeLinecap="round"
         strokeLinejoin="round"
         className="lucide lucide-rotate-ccw">
