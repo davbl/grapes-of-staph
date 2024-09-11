2e06947029748bdc6d18c1c939a305768d514f7f
akuataya <akuataja@gmail.com>
2024-09-11 19:09:33 +0200

    Add lucide icons as react package

--- src/App.jsx
+++ src/App.jsx
@@ -2,50 +2,14 @@ import Result from "./components/Result";
 import TraditionalMethods from "./components/TraditionalMethods";
 import MolecularMethods from "./components/MolecularMethods";
 import ResetBtn from "./components/ResetBtn";
+import { Grape } from "lucide-react";
 
 const App = () => {
   // Render
   return (
     <>
       <header>
-        <svg
-          width="13.5"
-          height="13.5"
-          viewBox="0 0 64 64"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg">
-          <ellipse
-            cx="32"
-            cy="45.4286"
-            rx="16"
-            ry="16"
-            transform="rotate(180 32 45.4286)"
-            fill="#9D70CC"
-          />
-          <ellipse
-            cx="16"
-            cy="18"
-            rx="16"
-            ry="16"
-            transform="rotate(180 16 18)"
-            fill="#9D70CC"
-          />
-          <ellipse
-            cx="48"
-            cy="18"
-            rx="16"
-            ry="16"
-            transform="rotate(180 48 18)"
-            fill="#9D70CC"
-          />
-          <rect
-            x="22.8572"
-            y="18"
-            width="18.2857"
-            height="18.2857"
-            fill="#9D70CC"
-          />
-        </svg>
+        <Grape size={18} color="#9E70CC" strokeWidth={2.25} />
         <h1>Grapes of Staph</h1>
       </header>
 
